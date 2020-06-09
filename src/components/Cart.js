918a9866ccf92e258f7db15c87b3be9f819914f7
Adrian <AdrianForde02@gmail.com>
2020-06-08 23:49:12 -0300

    Changed input field for quantity to number type and set a min number to 0

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -29,7 +29,7 @@ const Cart = () => {
   };
 
   const setClothingCost = () => {
-    carts.map(cart => {
+    carts.map((cart) => {
       if (cart.sale !== null)
         clothingCost =
           parseFloat(clothingCost) + parseFloat(cart.sale) * cart.quantity;
@@ -46,11 +46,11 @@ const Cart = () => {
     return total.toFixed(2);
   };
 
-  const changeQuantity = e => {
+  const changeQuantity = (e) => {
     const currentInputID = document.getElementById(
       'changeQuantity' + currentCartID
     );
-    carts.map(cart => {
+    carts.map((cart) => {
       if (cart.id === currentCartID) {
         if (parseInt(currentInputID.value) === 0) {
           console.log('is zero');
@@ -65,11 +65,11 @@ const Cart = () => {
     setClothingCost();
   };
 
-  const removeItem = cartID => {
+  const removeItem = (cartID) => {
     localStorage.setItem(
       'Cart',
       JSON.stringify(
-        carts.filter(cart => {
+        carts.filter((cart) => {
           if (
             cart.id === currentCartID &&
             cart.size === currentCartSize &&
@@ -86,43 +86,45 @@ const Cart = () => {
   };
 
   return carts.length > 0 ? (
-    <div className='container-fluid'>
+    <div className="container-fluid">
       <AnnouncementTag />
-      <div className='text-center mt-3'>
+      <div className="text-center mt-3">
         <h5>Cart</h5>
       </div>
-      <ul className='list-group mt-3'>
-        {carts.map(cart => {
+      <ul className="list-group mt-3">
+        {carts.map((cart) => {
           return (
-            <li className='list-group-item cart-container h-auto' key={uuid()}>
-              <div className='favorites-link'>
-                <div className='col-sm-3 mr-5 favorites-image-container'>
-                  <img src={cart.image} alt='' className='favorites-img' />
+            <li className="list-group-item cart-container h-auto" key={uuid()}>
+              <div className="favorites-link">
+                <div className="col-sm-3 mr-5 favorites-image-container">
+                  <img src={cart.image} alt="" className="favorites-img" />
                 </div>
-                <div className='favorite-info col-sm-9'>
+                <div className="favorite-info col-sm-9">
                   <h6>{cart.name}</h6>
                   <p>Price: ${cart.price} USD</p>
-                  <p className='sale'>
+                  <p className="sale">
                     {cart.sale !== null ? (
                       <span>Sale ${cart.sale} USD</span>
                     ) : (
                       <span></span>
                     )}
                   </p>
-                  <div className='display-items-size-container'>
+                  <div className="display-items-size-container">
                     <div key={uuid()}>{cart.size}</div>
                     <div
-                      className='show-item-col w-20 h-20'
+                      className="show-item-col w-20 h-20"
                       style={{ background: cart.color }}
                       key={uuid()}
                     ></div>
                   </div>
-                  <div className='w-auto h-auto mb-2'>
+                  <div className="w-auto h-auto mb-2">
                     <span>Quantity:</span>
                     <form onSubmit={preventReload}>
                       <input
-                        type='text'
-                        placeholder={cart.quantity}
+                        type="number"
+                        min="0"
+                        step="1"
+                        value={cart.quantity}
                         id={'changeQuantity' + cart.id}
                         onChange={() => {
                           currentCartID = cart.id;
@@ -132,7 +134,7 @@ const Cart = () => {
                     </form>
                   </div>
                   <button
-                    className='btn btn-danger btn-sm'
+                    className="btn btn-danger btn-sm"
                     onClick={() => {
                       currentCartID = cart.id;
                       currentCartColor = cart.color;
@@ -148,9 +150,9 @@ const Cart = () => {
           );
         })}
       </ul>
-      <div className='w-100 d-flex justify-content-end mt-3'>
+      <div className="w-100 d-flex justify-content-end mt-3">
         <button
-          className='btn btn-sm btn-danger mr-5'
+          className="btn btn-sm btn-danger mr-5"
           onClick={() => {
             localStorage.setItem('Cart', JSON.stringify([]));
             setCarts(JSON.parse(localStorage.getItem('Cart')));
@@ -159,35 +161,35 @@ const Cart = () => {
           Remove All
         </button>
       </div>
-      <div className='container-fluid w-100 d-flex justify-content-end align-items-center mb-1 mt-5'>
-        <div className='mr-0 mr-md-5'>
-          <span className='font-weight-bold mr-1'>Clothing Cost:</span> $
+      <div className="container-fluid w-100 d-flex justify-content-end align-items-center mb-1 mt-5">
+        <div className="mr-0 mr-md-5">
+          <span className="font-weight-bold mr-1">Clothing Cost:</span> $
           {setClothingCost()} USD
         </div>
       </div>
-      <div className='container-fluid w-100 d-flex justify-content-end align-items-center mb-1 mt-1'>
-        <div className='mr-0 mr-md-5'>
-          <span className='font-weight-bold mr-1'>Shipping Cost:</span> $
+      <div className="container-fluid w-100 d-flex justify-content-end align-items-center mb-1 mt-1">
+        <div className="mr-0 mr-md-5">
+          <span className="font-weight-bold mr-1">Shipping Cost:</span> $
           {shippingCost} USD
         </div>
       </div>
-      <div className='container-fluid w-100 d-flex justify-content-end align-items-center mb-5'>
-        <div className='mr-0 mr-md-5'>
-          <span className='font-weight-bold mr-1'>Total Cost:</span> $
+      <div className="container-fluid w-100 d-flex justify-content-end align-items-center mb-5">
+        <div className="mr-0 mr-md-5">
+          <span className="font-weight-bold mr-1">Total Cost:</span> $
           {setTotal()}
           USD
         </div>
       </div>
-      <div className='container-fluid w-100 d-flex flex-column align-items-center mb-5'>
-        <button className='btn btn-dark btn-lg text-center mt-5 pr-5 mx-auto'>
+      <div className="container-fluid w-100 d-flex flex-column align-items-center mb-5">
+        <button className="btn btn-dark btn-lg text-center mt-5 pr-5 mx-auto">
           <span>Proceed To Checkout</span>
         </button>
       </div>
     </div>
   ) : (
-    <div className='container-fluid'>
+    <div className="container-fluid">
       <AnnouncementTag />
-      <div className='container mt-5 cart-empty'>
+      <div className="container mt-5 cart-empty">
         There are currently no items in cart
       </div>
     </div>
